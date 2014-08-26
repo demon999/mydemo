5a4b5f5412dfe16283377d1e6f3ae2aeec84dc91
Richard.Xue <liming.snowy@gmail.com>
2014-08-26 09:34:43 +0800

    Submit config of Spring

--- nosql/src/main/java/com/snow/redis/Test.java
+++ nosql/src/main/java/com/snow/redis/Test.java
@@ -1,10 +1,15 @@
 package com.snow.redis;
 
+import redis.clients.jedis.JedisPoolConfig;
+
 /**
  * Created by Richard on 14-8-25.
  */
 public class Test {
     public static void main(String[] args) {
-
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxIdle(100);
+        config.setMinIdle(100);
+        config.setMaxWaitMillis(100000);
     }
 }
