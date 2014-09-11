59cba786826f690494ccffdc980e40ab08ed68cd
Richard.Xue <liming.snowy@gmail.com>
2014-09-11 10:29:32 +0800

    Add Callable

---
+++ base/src/main/java/com/snow/callable/CallableAndFuture.java
@@ -0,0 +1,29 @@
+package com.snow.callable;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * Created by Richard on 14-9-11.
+ */
+public class CallableAndFuture {
+    public static void main(String[] args) {
+        Callable<Integer> callable = new Callable<Integer>() {
+            public Integer call() throws Exception {
+                return new Random().nextInt(100);
+            }
+        };
+        FutureTask<Integer> future = new FutureTask<Integer>(callable);
+        new Thread(future).start();
+        try {
+            Thread.sleep(5000);// 可能做一些事情
+            System.out.println(future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+}
