236cdcb6182c37cd08354863215a8651f95e7fdc
Richard.Xue <liming.snowy@gmail.com>
2014-12-30 11:03:19 +0800

    完善redis的message功能

--- nosql/src/main/java/com/snow/redis/message/RedisDAOImpl.java
+++ nosql/src/main/java/com/snow/redis/message/RedisDAOImpl.java
@@ -1,14 +1,18 @@
 package com.snow.redis.message;
 
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import java.io.Serializable;
 
 /**
  * Created by Richard on 2014/12/29.
  */
+@Component("redisDao")
 public class RedisDAOImpl implements RedisDao {
 
+    @Inject
     private RedisTemplate<String, Object> redisTemplate = null;
 
     public RedisDAOImpl() {
