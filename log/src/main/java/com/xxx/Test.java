92eaa2c710bf4cb7f008154cb23b44284d4f1e1e
Richard Xue <liming.snowy@gmail.com>
2015-05-29 18:01:26 +0800

    add log about elastic search

---
+++ log/src/main/java/com/xxx/Test.java
@@ -0,0 +1,13 @@
+package com.xxx;
+
+/**
+ * @author Richard Xue
+ * @version 1.0
+ * @date 05/29/2015
+ * @description
+ */
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ log/src/main/java/com/xxx/es/Sample.java
@@ -0,0 +1,64 @@
+package com.xxx.es;
+
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.ImmutableSettings;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.node.Node;
+import org.elasticsearch.node.NodeBuilder;
+
+/**
+ * @author Richard Xue
+ * @version 1.0
+ * @date 05/29/2015
+ * @description
+ */
+public class Sample {
+    public static void main(String[] args) {
+        Sample sample = new Sample();
+        sample.test();
+    }
+
+    public void test() {
+        Node node = NodeBuilder.nodeBuilder().node();
+        Client client = node.client();
+        client.close();
+        node.close();
+    }
+
+    public void test2() {
+        Node node = NodeBuilder.nodeBuilder().clusterName("yourclustername").node();
+        Client client = node.client();
+    }
+
+    public void test3() {
+        Node node = NodeBuilder.nodeBuilder().client(true).node();
+        Client client = node.client();
+        client.close();
+        node.close();
+    }
+
+    public void test4() {
+        Node node = NodeBuilder.nodeBuilder().local(true).node();
+        Client client = node.client();
+        client.close();
+        node.close();
+    }
+
+    public void test5() {
+        Client client = new TransportClient()
+                .addTransportAddress(new InetSocketTransportAddress("host1", 9300))
+                .addTransportAddress(new InetSocketTransportAddress("host2", 9300));
+        // on shutdown
+        client.close();
+    }
+
+    public void test6() {
+        Settings settings = ImmutableSettings.settingsBuilder().put("cluster.name", "myClusterName").build();
+        Client client =    new TransportClient(settings);
+
+//        Settings settings = ImmutableSettings.settingsBuilder().put("client.transport.sniff", true).build();
+//        TransportClient client = new TransportClient(settings);
+    }
+}
