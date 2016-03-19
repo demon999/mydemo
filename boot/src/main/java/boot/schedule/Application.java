cdb8bf663c981addc383461bc582b76ad4f2dd51
xueliming <liming.snowy@gmail.com>
2016-03-19 18:00:09 +0800

    add spring boot sample

---
+++ boot/src/main/java/boot/schedule/Application.java
@@ -0,0 +1,14 @@
+package com.snow.boot.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+/**
+ * Created by Richard on 14-8-26.
+ */
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(ScheduledTasks.class);
+    }
+}
---
+++ boot/src/main/java/boot/schedule/ScheduledTasks.java
@@ -0,0 +1,24 @@
+package com.snow.boot.schedule;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Richard on 14-8-26.
+ */
+@EnableAutoConfiguration
+@EnableScheduling
+public class ScheduledTasks {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 5000)
+    public void reportCurrentTime() {
+        System.out.println("The time is now " + dateFormat.format(new Date()));
+    }
+
+}
