09eb800a0071cf8cb98b1bc26175cb23ac1179de
richard.xue <liming.snowy@gmail.com>
2018-08-06 18:21:53 +0800

    add Proxy sample

---
+++ base/src/main/java/com/snow/reflect/CglibProxyTest.java
@@ -0,0 +1,40 @@
+package com.snow.reflect;
+
+import net.sf.cglib.proxy.Enhancer;
+import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibProxyTest {
+
+    public static void main(String[] args) {
+        CglibProxy cglibProxy = new CglibProxy();
+        MyInterface myInterface = (MyInterface) cglibProxy.getCglibProxy(new MyInterfaceImpl());
+        myInterface.say("world!");
+    }
+
+}
+
+class CglibProxy implements MethodInterceptor {
+    private Object target;//需要代理的目标对象
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("Cglib动态代理，监听开始！");
+        Object invoke = method.invoke(target, objects);//方法执行，参数：target 目标对象 arr参数数组
+        System.out.println("Cglib动态代理，监听结束！");
+        return invoke;
+    }
+
+    public Object getCglibProxy(Object objectTarget){
+        //为目标对象target赋值
+        this.target = objectTarget;
+        Enhancer enhancer = new Enhancer();
+        //设置父类,因为Cglib是针对指定的类生成一个子类，所以需要指定父类
+        enhancer.setSuperclass(objectTarget.getClass());
+        enhancer.setCallback(this);// 设置回调
+        Object result = enhancer.create();//创建并返回代理对象
+        return result;
+    }
+}
