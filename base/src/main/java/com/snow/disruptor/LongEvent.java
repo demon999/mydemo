b45816b8925fde900901c4bf782c82bfec9ff4f6
richard.xue <liming.snowy@gmail.com>
2018-08-17 23:52:39 +0800

    disruptor

---
+++ base/src/main/java/com/snow/disruptor/LongEvent.java
@@ -0,0 +1,13 @@
+package com.snow.disruptor;
+
+public class LongEvent {
+    private long value;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+}
