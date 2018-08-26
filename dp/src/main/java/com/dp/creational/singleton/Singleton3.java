74e74662c93e1647d7796ff1c9bc3743bdf99998
richard.xue <liming.snowy@gmail.com>
2018-08-26 11:36:40 +0800

    update design pattern

---
+++ dp/src/main/java/com/dp/creational/singleton/Singleton3.java
@@ -0,0 +1,9 @@
+package com.dp.creational.singleton;
+
+public enum  Singleton3 {
+    INSTANCE;
+
+    public void aa() {
+        System.out.println("aa");
+    }
+}
--- dp/src/main/java/com/dp/structural/adapter/Targetable.java
+++ dp/src/main/java/com/dp/structural/adapter/Targetable.java
@@ -6,7 +6,7 @@ package com.dp.structural.adapter;
  */
 public interface Targetable {
     //与源类相同的接口函数
-    public void operation1();
+    void operation1();
     //新的接口函数,源类中没有
-    public void operation2();
+    void operation2();
 }
