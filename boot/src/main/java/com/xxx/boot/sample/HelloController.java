7d7f87a339ca6b2e5c185be2af62b4f160a0579a
richard.xue <liming.snowy@gmail.com>
2018-08-26 11:52:32 +0800

    rename package name

---
+++ boot/src/main/java/com/xxx/boot/sample/HelloController.java
@@ -0,0 +1,17 @@
+package com.xxx.boot.sample;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Richard on 14-8-26.
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/index")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ boot/src/main/java/com/xxx/boot/sample/SimpleController.java
@@ -0,0 +1,24 @@
+package com.xxx.boot.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Administrator on 14-6-24.
+ */
+@RestController
+@EnableAutoConfiguration
+public class SimpleController {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String hello() {
+        return "hello world!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleController.class,args);
+    }
+}
