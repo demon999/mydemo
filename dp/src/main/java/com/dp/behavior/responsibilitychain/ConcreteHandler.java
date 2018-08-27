aeb074f69825acf07e43732ef11d62236ef1f87c
richard.xue <liming.snowy@gmail.com>
2018-08-27 22:20:20 +0800

    responsibility chain

---
+++ dp/src/main/java/com/dp/behavior/responsibilitychain/ConcreteHandler.java
@@ -0,0 +1,18 @@
+package com.dp.behavior.responsibilitychain;
+
+public class ConcreteHandler extends Handler {
+    @Override
+    public void handleRequest() {
+        /**
+         * 判断是否有后继的责任对象
+         * 如果有，就转发请求给后继的责任对象
+         * 如果没有，则处理请求
+         */
+        if (getSuccessor() != null) {
+            System.out.println("放过请求");
+            getSuccessor().handleRequest();
+        } else {
+            System.out.println("处理请求");
+        }
+    }
+}
