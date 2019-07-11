8e24a3253a65054f4cfb9167194c4b5304e94402
richard.xue <richard_xue@icloud.com>
2019-07-11 15:15:42 +0800

    更新包

--- base/src/main/java/com/xml/Test.java
+++ base/src/main/java/com/xml/Test.java
@@ -1,5 +1,8 @@
 package com.xml;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @author Richard Xue
  * @version 1.0
@@ -9,6 +12,15 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println(5 ^ 0);
+
+        Set<Short> sets = new HashSet<>(100);
+        for (int i = 0; i < 100; i++) {
+            sets.add((short)i);
+            sets.remove((short)(i-1));
+        }
+        System.out.println(sets.size());
+
+
     }
 
 }
